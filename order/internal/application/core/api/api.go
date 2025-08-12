df008739c136b71cd4cf14bd728d33fe77616737
Samuel-MN0211 <smerson0211@gmail.com>
2025-08-12 15:06:21 -0300

    grpc 3

--- order/internal/application/core/api/api.go
+++ order/internal/application/core/api/api.go
@@ -3,6 +3,8 @@ package api
 import (
 	"github.com/ruandg/microservices/order/internal/application/core/domain"
 	"github.com/ruandg/microservices/order/internal/ports"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Application struct {
@@ -18,13 +20,32 @@ func NewApplication(db ports.DBPort, payment ports.PaymentPort) *Application {
 }
 
 func (a Application) PlaceOrder(order domain.Order) (domain.Order, error) {
+	var totalQuantity int32
+	for _, item := range order.OrderItems {
+		totalQuantity += item.Quantity
+	}
+	if totalQuantity > 50 {
+		order.Status = "Canceled"
+		a.db.Save(&order)
+		return domain.Order{}, status.Errorf(codes.InvalidArgument, "Order cannot be placed: total quantity (%d items) exceeds maximum allowed limit of 50 items", totalQuantity)
+	}
+
 	err := a.db.Save(&order)
 	if err != nil {
+		order.Status = "Canceled"
+		a.db.Save(&order)
 		return domain.Order{}, err
 	}
+
 	paymentErr := a.payment.Charge(order)
 	if paymentErr != nil {
+		order.Status = "Canceled"
+		a.db.Save(&order)
 		return domain.Order{}, paymentErr
 	}
+
+	order.Status = "Paid"
+	a.db.Save(&order)
+
 	return order, nil
 }
